5f5718e8a560e206cf37060a8237a5f73c5d6b16
qb <kalyan.raparthi@hotmail.com>
2023-03-06 02:16:13 +0530

    [INITIAL COMMIT]

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const mongoose = require("mongoose");
+const app = express()
+
+const URI = "mongodb+srv://qb:rootqb@files-cluster.xdcz95b.mongodb.net/messagesbase?retryWrites=true&w=majority";
+const ID = "369"
+
+const MESSAGE = mongoose.model("Message", new mongoose.Schema({
+    _id: String,
+    data: String
+}));
+mongoose.connect(URI,() => {
+    console.log("[ATLAS IS LIVE]");});
+
+MESSAGE.find({_id: ID}, (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        console.log(data[0].data);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send(respone)
+// })
+// app.listen(process.env.PORT || 3000)
+// console.log("Listening for requests...");
