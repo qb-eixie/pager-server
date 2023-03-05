b8987c6960bab496b4ba2f77d410ca1cd87ed26e
qb <kalyan.raparthi@hotmail.com>
2023-03-06 02:55:45 +0530

    asyn added

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ const app = express()
 
 const URI = "mongodb+srv://qb:rootqb@files-cluster.xdcz95b.mongodb.net/messagesbase?retryWrites=true&w=majority";
 const ID = "369"
-
 const MESSAGE = mongoose.model("Message", new mongoose.Schema({
     _id: String,
     data: String
 }));
-mongoose.connect(URI,() => {
-    console.log("[ATLAS IS LIVE]");});
 
-MESSAGE.find({_id: ID}, (err, data) => {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        console.log(data[0].data);
-    }
+mongoose.connect(URI,() => {
+    console.log("[base connected]");
+    http();
 });
+
+function find() {
+    MESSAGE.find({_id: ID}, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            return(data[0].data);
+        }
+    });
+}
+
+function http() {
+    app.all('/', (req, res) => {
+        console.log("Just got a request!")
+        res.send(find())
+    })
+    app.listen(process.env.PORT || 3000)
+}
 
 
 
