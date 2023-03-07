e4e1a461f09c55cd7138d8c3e12ad4876c8f0063
qb <kalyan.raparthi@hotmail.com>
2023-03-08 01:43:27 +0530

    Set-ONE.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //[dependecies]
 const express = require('express');
 const fs = require("fs");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
 //[middelware funtions]
@@ -10,55 +10,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+mongoose.set('strictQuery', true)
 
+const URI = "mongodb+srv://qb:rootqb@files-cluster.xdcz95b.mongodb.net/messagesbase?retryWrites=true&w=majority";
+const ID = "369"
 
+//[Mongoose Schema]
+const MESSAGE = mongoose.model("Message", new mongoose.Schema({
+    _id: String,
+    data: String
+}));
+
+//[Establishing Connection with ATLAS.]
+mongoose.connect(URI,() => {
+    console.log("[ATLAS CONNECTED]"); 
+});
+
+app.get('/m', (request, response) => {
+        const msg = MESSAGE.find({_id: ID});
+        msg.then(data => {
+        response.send(data[0].data);
+    })
+})
 
 app.post('/', (req, res) => {
-    const formData = req.body;
-    fs.appendFile(__dirname + "/history.txt", formData.msg, (err) => {
-        if(err) {
-            console.log(err);
+    const formData = String(req.body.msg);
+    console.log("Response:"+ formData);
+    MESSAGE.replaceOne({_id: ID}, {data: formData}, (a) => {
+        if(a) {
+            console.log(a);
         }
-    });
-    console.log(formData);
-    fs.writeFile("messages.txt", formData.msg, (err) => {
-        if(err) {
-            console.log(err);
-        }
-    });
+    })
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get('/msg', (request, response) => {
-    response.sendFile(__dirname + "/msg.txt")
-});
+
 
 app.listen(3000, () => {
-    console.log('LISTENTING');
+    console.log("[LISTENTING]");
 });
 
 
-
-// const URI = "mongodb+srv://qb:rootqb@files-cluster.xdcz95b.mongodb.net/messagesbase?retryWrites=true&w=majority";
-// const ID = "369"
-// //[Mongoose Schema]
-// const MESSAGE = mongoose.model("Message", new mongoose.Schema({
-//     _id: String,
-//     data: String
-// }));
-
-//[Establishing Connection with ATLAS.]
-// mongoose.connect(URI,() => {
-//     console.log("[Base Connected]"); 
-//     // postfn();
-// });
-
-// MESSAGE.find({_id: ID})
-// .then(err => {
-//     if(err) {
-//         console.log(err);
-//     }})
-// .then(data => {
-
-// });
 
